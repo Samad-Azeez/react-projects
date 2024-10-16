b53e6ae3eaf35ae7441cdda1984961b33c63f69b
Samad-Azeez <azeezsamad2004@gmail.com>
2024-10-16 15:25:00 +0100

    Task: Refactor App and List components to display a list of birthdays

--- 1-birthday-reminder/src/List.jsx
+++ 1-birthday-reminder/src/List.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import data from './data';
 
 const List = () => {
-  return <div>List</div>;
+  const [people, setPeople] = useState(data);
+  const count = people.length;
+
+  return (
+    <div>
+      <h2>{count} Birthdays Today</h2>
+      <div>
+        {/* Display the list of people */}
+        {people.map((person) => {
+          const { id, name, age, image } = person;
+          return (
+            <div key={id}>
+              <img src={image} alt='person image' style={{ width: '80px' }} />
+              <h4>{name}</h4>
+              <p>{age}</p>
+            </div>
+          );
+        })}
+        {/* Clear All button */}
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </div>
+    </div>
+  );
 };
 
 export default List;
