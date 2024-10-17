c97842646e078df9dbbd3c23d324a7e8ec678b84
Samad-Azeez <azeezsamad2004@gmail.com>
2024-10-17 15:13:05 +0100

    Refactor App and Question components to implement accordion functionality

--- 4-accordion/src/App.jsx
+++ 4-accordion/src/App.jsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+import { useState } from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  const [questions, setQuestions] = useState(data);
+
+  return (
+    <main>
+      <div className='container'>
+        <h3>Questions and answers about login</h3>
+        <section className='info'>
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 };
 
 export default App;
