0b6c514041ec213451a20984b7aa2b19bf79ce27
Samad-Azeez <azeezsamad2004@gmail.com>
2024-10-17 15:38:52 +0100

    Refactor App component to include menu items and categories

--- 5-menu/src/App.jsx
+++ 5-menu/src/App.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
 
 const App = () => {
-  return <div>App</div>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 
 export default App;
--- 5-menu/src/Menu.jsx
+++ 5-menu/src/Menu.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
 
-const Menu = () => {
-  return <h2>menu component</h2>;
+const Menu = (props) => {
+  const { items } = props;
+
+  return (
+    <div className='section-center'>
+      {items.map((menuItem) => {
+        const { id, title, img, desc, price } = menuItem;
+
+        return (
+          <article key={id} className='menu-item'>
+            <img src={img} alt={title} className='photo' />
+            <div className='item-info'>
+              <header>
+                <h4>{title}</h4>
+                <h4 className='price'>${price}</h4>
+              </header>
+            </div>
+            <p className='item-text'>{desc}</p>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Menu;
