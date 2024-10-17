fa9b25f09330eb8279974d525d7a6c76ac56d733
Samad-Azeez <azeezsamad2004@gmail.com>
2024-10-17 18:16:27 +0100

    feat: Refactor package.json and package-lock.json to include react-icons dependency

--- 6-tabs/src/App.jsx
+++ 6-tabs/src/App.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { FaAngleDoubleRight } from 'react-icons/fa';
 
 const url = 'https://www.course-api.com/react-tabs-project';
 
@@ -51,7 +51,48 @@ const App = () => {
     );
   }
 
-  return <main></main>;
+  const { company, dates, duties, title } = jobs[value];
+
+  return (
+    <section className='section'>
+      <div className='title'>
+        <h2>Experience</h2>
+        <div className='underline'></div>
+      </div>
+      <div className='jobs-center'>
+        {/* btn container */}
+        <div className='btn-container'>
+          {jobs.map((job, index) => {
+            return (
+              <button
+                key={job.id}
+                onClick={() => setValue(index)} // set value to index when button is clicked
+                className={`job-btn ${index === value && 'active-btn'}`} // if index is equal to value, add active-btn class
+              >
+                {job.company} {/* display company name */}
+              </button>
+            );
+          })}
+        </div>
+
+        {/* job info */}
+        <article className='job-info'>
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          <p className='job-date'>{dates}</p>
+          {duties.map((duty, index) => {
+            // iterate over duties array and display each duty
+            return (
+              <div key={index} className='job-desc'>
+                <FaAngleDoubleRight className='job-icon' />
+                <p>{duty}</p>
+              </div>
+            );
+          })}
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default App;
