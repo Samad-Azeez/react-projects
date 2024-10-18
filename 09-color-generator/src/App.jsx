161018c2d640d1714bf0264b0d13aa722a94e3b1
Samad-Azeez <azeezsamad2004@gmail.com>
2024-10-18 16:51:54 +0100

    Refactor App component to include form submission and display functionality

--- 09-color-generator/src/App.jsx
+++ 09-color-generator/src/App.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import Values from 'values.js';
+import SingleColor from './SingleColor';
 import { useState, useEffect } from 'react';
 
 const App = () => {
-  return <div>App</div>;
+  const [color, setColor] = useState('');
+  const [isError, setIsError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // set isError to false to remove the error message if there is any before
+    setIsError(false);
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      // if there is an error, set isError to true
+      setIsError(true);
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={isError ? 'error' : null}
+          />
+          <button type='submit' className='btn'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          console.log(color);
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
+      </section>
+    </>
+  );
 };
 
 export default App;
