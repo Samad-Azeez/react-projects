f305b9aef2cfc9b74ce118c0be22b338d989e720
Samad-Azeez <azeezsamad2004@gmail.com>
2024-10-21 16:39:20 +0100

    Refactor Alert component to include list prop in useEffect dependency array

--- 10-grocery-bud/src/Alert.jsx
+++ 10-grocery-bud/src/Alert.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect } from 'react';
 
-const Alert = ({ type, msg, removeAlert }) => {
+const Alert = (props) => {
+  const { type, msg, removeAlert, list } = props;
+
   useEffect(() => {
     const timeout = setTimeout(() => {
       removeAlert();
     }, 3000);
     return () => clearTimeout(timeout);
-  }, []);
+  }, [list]);
 
   return <p className={`alert alert-${type}`}>{msg}</p>; // set the class name based on the type prop passed in from the parent component (App)
 };
