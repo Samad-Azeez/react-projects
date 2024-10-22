5003cc92032c53397684f846a0bd513b0ddd0086
Samad-Azeez <azeezsamad2004@gmail.com>
2024-10-22 13:42:18 +0100

    Remove Sidebar component from App

--- 11-navbar/src/App.jsx
+++ 11-navbar/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Navbar from './Navbar';
-import Sidebar from './Sidebar';
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Sidebar />
     </>
   );
 };
