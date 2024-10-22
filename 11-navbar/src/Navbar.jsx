3ed13aadbc502ab5e5a4271870fd758d1c433898
Samad-Azeez <azeezsamad2004@gmail.com>
2024-10-22 13:40:13 +0100

    feat: Add react-icons package to project dependencies

--- 11-navbar/src/Navbar.jsx
+++ 11-navbar/src/Navbar.jsx
@@ -1,10 +1,46 @@
-import React, { useState, useRef, useEffect } from 'react'
-import { FaBars, FaTwitter } from 'react-icons/fa'
-import { links, social } from './data'
-import logo from './logo.svg'
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import { links, social } from './data';
+import logo from './assets/logo.svg';
 
 const Navbar = () => {
-  return <h4>navbar</h4>
-}
+  return (
+    <nav>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' />
+          {/* only show the button when the screen size is less than 992px */}
+          <button className='nav-toggle'>
+            <FaBars />
+          </button>
+        </div>
+        {/*  Add the links here */}
+        <div className='links-container show-container'>
+          <ul className='links'>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        {/* Add the social icons here */}
+        <ul className='social-icons'>
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
