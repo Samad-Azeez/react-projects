0ad8d627fb4a793bc80530be4c24d9bca2450464
Samad-Azeez <azeezsamad2004@gmail.com>
2024-10-22 14:32:40 +0100

    Refactor Navbar component to dynamically adjust the height of the links container

--- 11-navbar/src/Navbar.jsx
+++ 11-navbar/src/Navbar.jsx
@@ -4,19 +4,36 @@ import { links, social } from './data';
 import logo from './assets/logo.svg';
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  // this useEffect will run every time the value of showLinks changes to set the height of the links container to the height of the links
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height; // get the height of the links
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`; // set the height of the container to the height of the links
+    } else {
+      linksContainerRef.current.style.height = '0px'; // set the height of the container to 0
+    }
+  }, [showLinks]);
   return (
     <nav>
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} alt='logo' />
           {/* only show the button when the screen size is less than 992px */}
-          <button className='nav-toggle'>
+          <button
+            className='nav-toggle'
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <FaBars />
           </button>
         </div>
+
         {/*  Add the links here */}
-        <div className='links-container show-container'>
-          <ul className='links'>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
@@ -27,6 +44,7 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+
         {/* Add the social icons here */}
         <ul className='social-icons'>
           {social.map((socialIcon) => {
