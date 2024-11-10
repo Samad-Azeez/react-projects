274a3e705b7f2c49e0a567c9ed7f97a9dcfd7702
Samad-Azeez <azeezsamad2004@gmail.com>
2024-11-10 22:37:30 +0100

    feat: Implement Navbar component with global context integration and submenu functionality

--- 13-stripe-submenus/src/Navbar.jsx
+++ 13-stripe-submenus/src/Navbar.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import logo from './assets/images/logo.svg';
 import { FaBars } from 'react-icons/fa';
+import { useGlobalContext } from './context';
 
 const Navbar = () => {
-  return <h2>navbar component</h2>;
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+
+  return (
+    <nav className='nav'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} className='nav-logo' alt='stripe' />
+          <button className='btn toggle-btn' onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+
+        {/* Add onMouseOver event listener to openSubmenu */}
+        <ul className='nav-links'>
+          <li>
+            <button className='link-btn' onMouseOver={openSubmenu}>
+              products
+            </button>
+          </li>
+          <li>
+            <button className='link-btn' onMouseOver={openSubmenu}>
+              developers
+            </button>
+          </li>
+          <li>
+            <button className='link-btn' onMouseOver={openSubmenu}>
+              company
+            </button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
