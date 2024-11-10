86ce7f88e22d6f1033a7ea6ddcdec3a7c66399fa
Samad-Azeez <azeezsamad2004@gmail.com>
2024-11-10 21:27:58 +0100

    Refactor Hero and Navbar components

--- 13-stripe-submenus/src/context.jsx
+++ 13-stripe-submenus/src/context.jsx
@@ -1,2 +1,42 @@
-import React, { useState, useContext } from 'react'
-import sublinks from './data'
+import { useState, useContext, createContext } from 'react';
+import sublinks from './data';
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useContext(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        closeSidebar,
+        openSubmenu,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export { AppProvider };
