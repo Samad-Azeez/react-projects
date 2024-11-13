6cad40eaf29e41adb86841187b3311264d762358
Samad-Azeez <azeezsamad2004@gmail.com>
2024-11-13 20:04:45 +0100

    feat: Add mouse leave functionality to Submenu for improved user experience

--- 13-strapi-submenus/src/Submenu.jsx
+++ 13-strapi-submenus/src/Submenu.jsx
@@ -1,13 +1,39 @@
+import { useRef } from 'react';
 import { useGlobalContext } from './context';
 import sublinks from './data';
 
 const Submenu = () => {
-  const { pageId } = useGlobalContext();
+  const { pageId, setPageId } = useGlobalContext();
   // Find the current page in the sublinks array
   const currentPage = sublinks.find((item) => item.pageId === pageId);
 
+  // Create a reference to the submenu container
+  const submenuContainer = useRef(null);
+
+  // function to handle mouse leave event
+  const handleMouseLeave = (event) => {
+    const submenu = submenuContainer.current; // Get the submenu container element from the ref object
+    // Get the dimensions of the submenu container
+    const result = submenu.getBoundingClientRect();
+    // get the X and Y coordinates of the mouse pointer from the event object when the mouse leaves the submenu
+    const { clientX, clientY } = event;
+
+    // Check if the mouse pointer is outside the submenu container
+    if (
+      clientX < result.left ||
+      clientX > result.right ||
+      clientY > result.bottom
+    ) {
+      setPageId(null);
+    }
+  };
+
   return (
-    <div className={currentPage ? `submenu show-submenu` : `submenu`}>
+    <div
+      className={currentPage ? `submenu show-submenu` : `submenu`}
+      onMouseLeave={handleMouseLeave}
+      ref={submenuContainer}
+    >
       <h5>{currentPage?.page}</h5>
       <div
         className='submenu-links'
