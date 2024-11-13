b5baa0cb9ecf049c2c7f32af4e59ea2bfee9d3fa
Samad-Azeez <azeezsamad2004@gmail.com>
2024-11-13 18:45:19 +0100

    feat: Implement Submenu component to display links based on current pageId

--- 13-strapi-submenus/src/Submenu.jsx
+++ 13-strapi-submenus/src/Submenu.jsx
@@ -1,7 +1,35 @@
-import { useState, useRef, useEffect } from 'react';
+import { useGlobalContext } from './context';
+import sublinks from './data';
 
 const Submenu = () => {
-  return <h2>submenu component</h2>;
+  const { pageId } = useGlobalContext();
+  // Find the current page in the sublinks array
+  const currentPage = sublinks.find((item) => item.pageId === pageId);
+
+  return (
+    <div className='submenu'>
+      <h5>{currentPage?.page}</h5>
+      <div
+        className='submenu-links'
+        // Set the number of columns based on the number of links in the current page object
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? `1fr 1fr` : `1fr`,
+        }}
+      >
+        {currentPage?.links?.map((link) => {
+          // Destructure the link object
+          const { id, label, icon, url } = link;
+          return (
+            <a key={id} href={url}>
+              {icon}
+              {label}
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Submenu;
