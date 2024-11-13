4af4994ed27ec928005417523f723b8d7af5fb5c
Samad-Azeez <azeezsamad2004@gmail.com>
2024-11-13 12:55:30 +0100

    feat: Update Hero component layout and styles for improved content presentation

--- 13-strapi-submenus/src/Hero.jsx
+++ 13-strapi-submenus/src/Hero.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import phoneImg from './assets/images/phone.svg';
-import { useGlobalContext } from './context.jsx';
-
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log(data);
-  return <h2>hero component</h2>;
+  return (
+    <div className='hero-container'>
+      <div className='hero-center'>
+        <h1>
+          Manage any content <br /> Anywhere
+        </h1>
+        <p>
+          Strapi is the leading open-source headless CMS. It's 100% javascript
+          and fully customizable
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
