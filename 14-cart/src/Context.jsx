18843c3a1f3db38a1781a70f984a0cc3e2634d4a
Samad-Azeez <azeezsamad2004@gmail.com>
2024-11-13 23:38:06 +0100

    feat: Implement global state management with AppContext and reducer

--- 14-cart/src/Context.jsx
+++ 14-cart/src/Context.jsx
@@ -1,9 +1,19 @@
 import { createContext, useContext, useReducer, useEffect } from 'react';
+import reducer from './reducer';
 
 const AppContext = createContext();
 
+const initialState = {
+  loading: false,
+  cart: [],
+};
+
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+  );
 };
 
 const useGlobalContext = () => {
