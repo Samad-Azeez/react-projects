a434428e798e995391c22b9edf9d2f03654e2da9
Samad-Azeez <azeezsamad2004@gmail.com>
2024-11-13 23:29:31 +0100

    feat: Add AppContext and AppProvider for global state management

---
+++ 14-cart/src/Context.jsx
@@ -0,0 +1,13 @@
+import { createContext, useContext, useReducer, useEffect } from 'react';
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
+};
+
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppProvider };
