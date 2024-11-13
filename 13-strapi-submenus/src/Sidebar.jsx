3b3ececba34743aa638c92c5b7328dc846c69b69
Samad-Azeez <azeezsamad2004@gmail.com>
2024-11-13 13:34:20 +0100

    feat: Implement Sidebar component with dynamic sublinks and close functionality

--- 13-strapi-submenus/src/Sidebar.jsx
+++ 13-strapi-submenus/src/Sidebar.jsx
@@ -1,8 +1,41 @@
 import { FaTimes } from 'react-icons/fa';
 import sublinks from './data';
+import { useGlobalContext } from './context.jsx';
 
 const Sidebar = () => {
-  return <h2>sidebar component</h2>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+
+  return (
+    <aside className='sidebar'>
+      <div className='sidebar-container'>
+        <button className='close-btn'>
+          <FaTimes />
+        </button>
+        <div className='sidebar-links'>
+          {sublinks.map((item) => {
+            // Destructure the properties from the item object
+            const { pageId, page, links } = item;
+            return (
+              <article key={pageId}>
+                <h4>{page}</h4>
+                <div className='sidebar-sublinks'>
+                  {links.map((link) => {
+                    const { id, label, icon, url } = link;
+                    return (
+                      <a href={url} key={id}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
 };
 
 export default Sidebar;
